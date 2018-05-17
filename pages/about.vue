415b81884c74255b669ed8a48d3dabcec02f54be
Roger <roger.e.clayton@gmail.com>
2018-05-17 09:37:51 -0600

    experimenting with css grid

--- pages/about.vue
+++ pages/about.vue
@@ -1,17 +1,45 @@
 <template>
-  <div>
-  <div class="outlined">
-    <p class="this">stuff</p>
-  </div>
-  <div class="outlined">
-    
-  </div>
-  <div class="outlined">
-    
-  </div>
-  <div class="outlined">
-    
-  </div>
+  <div class="wrapper">
+    <div class="a">
+      <h1 class="title">About Roger</h1>
+    </div>
+    <div class="b">
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+      <p class="title">Rabbits</p>
+    </div>
+    <div class="c">Jake is the footer now</div>
+
+
   </div>
 </template>
 
@@ -22,7 +50,30 @@
 </script>
 
 <style scoped>
-  .this {
-    color: blue;
+  .wrapper {
+    display: grid;
+    grid-template-columns: 0.20fr 0.20fr 0.20fr 0.20fr 0.20fr;
+    grid-template-rows: 30vh auto 30vh;
+    grid-template-areas: 
+      "header header header header header"
+      ". main main main ."
+      "footer footer footer footer footer";
+    background: black;
+    padding: 10%;
   }
+
+  div {
+    background: white;
+  }
+
+  .a {
+    grid-area: header;
+  }
+  .b {
+    grid-area: main;
+  }
+  .c {
+    grid-area: footer;
+  }
+
 </style>
